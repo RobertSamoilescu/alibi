189cd6a321b620abccc7c73a7daf46a9444a1b5c
RobertSamoilescu <robert.samoilescu@gmail.com>
2022-04-08 15:16:26 +0100

    quick experiment with linspace discretization

--- alibi/utils/discretizer.py
+++ alibi/utils/discretizer.py
@@ -8,6 +8,80 @@ from typing import Dict, Callable, List, Sequence, Union
 class Discretizer(object):
 
     def __init__(self, data: np.ndarray, numerical_features: List[int], feature_names: List[str],
+                 percentiles=4) -> None:
+        self.to_discretize = numerical_features
+        self.nums = percentiles
+
+        bins = self.bins(data)
+        bins = [np.unique(x) for x in bins]
+
+        self.feature_intervals = {}  # type: Dict[int, list]
+        self.lambdas = {}            # type: Dict[int, Callable]
+        for feature, linspace in zip(self.to_discretize, bins):
+            # get nb of borders (nb of bins - 1) and the feature name
+            n_bins = linspace.shape[0]
+            name = feature_names[feature]
+
+            # create names for bins of discretized features
+            self.feature_intervals[feature] = ['%s <= %.2f' % (name, linspace[0])]
+            for i in range(n_bins - 1):
+                self.feature_intervals[feature].append('%.2f < %s <= %.2f' % (linspace[i], name, linspace[i + 1]))
+            self.feature_intervals[feature].append('%s > %.2f' % (name, linspace[n_bins - 1]))
+            self.lambdas[feature] = partial(self.get_percentiles, linspace=linspace)
+
+    @staticmethod
+    def get_percentiles(x: np.ndarray, linspace: np.ndarray) -> np.ndarray:
+        if len(linspace.shape) != 1:
+            raise ValueError("Expected 1D quantiles array!")
+        if not issorted(linspace):
+            raise ValueError("Linspace array should be sorted!")
+        return np.searchsorted(linspace, x)
+
+    def bins(self, data: np.ndarray) -> List[np.ndarray]:
+        """
+        Parameters
+        ----------
+        data
+            Data to discretize.
+
+        Returns
+        -------
+        List with bin values for each feature that is discretized.
+        """
+
+        bins = []
+        for feature in self.to_discretize:
+            min_val, max_val = np.min(data[:, feature]), np.max(data[:, feature])
+            linspace = np.linspace(start=min_val, stop=max_val, num=self.nums)
+            bins.append(linspace)
+
+        return bins
+
+    def discretize(self, data: np.ndarray) -> np.ndarray:
+        """
+        Parameters
+        ----------
+        data
+            Data to discretize.
+
+        Returns
+        -------
+        Discretized version of data with the same dimension.
+        """
+
+        data_disc = data.copy()
+        for feature in self.lambdas:
+            if len(data.shape) == 1:
+                data_disc[feature] = int(self.lambdas[feature](data_disc[feature]))
+            else:
+                data_disc[:, feature] = self.lambdas[feature](data_disc[:, feature]).astype(int)
+
+        return data_disc
+
+
+class OrigDiscretizer(object):
+
+    def __init__(self, data: np.ndarray, numerical_features: List[int], feature_names: List[str],
                  percentiles: Sequence[Union[int, float]] = (25, 50, 75)) -> None:
         """
         Initialize the discretizer.
